3a12ed4348c268b8e83fce8e2d8987acbd5fb253
Wesley Anjos <wanjos.andrade@gmail.com>
2021-04-30 17:35:12 -0300

    Ajuste estilizacao e estrutura do Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,19 @@
-import styles from './styles.module.scss'
+import styles from "./styles.module.scss";
 
 export default function Header() {
   return (
     <header className={styles.headerContainer}>
       <div>
-        <img src="/logo.svg" alt=""/>
+        <img src="/logo.svg" alt="" />
       </div>
 
       <div className={styles.location}>
-        <p>
-          Você está em
-          <div className={styles.locationPicked}>
-            <span>Escolha um Estado</span>
-            <img src="/edit.svg" alt="Botão para editar Estado"/>
-          </div>
-        </p>
+        Você está em
+        <div className={styles.locationPicked}>
+          <span>Escolha um Estado</span>
+          <img src="/edit.svg" alt="Botão para editar Estado" />
+        </div>
       </div>
     </header>
-  )
+  );
 }
