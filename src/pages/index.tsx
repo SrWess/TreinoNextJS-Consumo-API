a2985a2198d32a1c51978e07611dce1ec18f0823
Wesley Anjos <wanjos.andrade@gmail.com>
2021-04-30 17:36:04 -0300

    Criacao da pagina Home e realizado consumo inicial da API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,70 @@
-import Head from 'next/head'
+import { GetStaticProps } from "next";
+import Head from "next/head";
+import CardCarAvailable from "../components/CardCarAvailable";
 
-export default function Home() {
+import styles from "./home.module.scss";
+
+type InfoCar = {
+  model: string;
+  marketing_name: string;
+  logo_dark: string;
+  thumb: string;
+};
+
+type HondaProps = {
+  carsAvailable: InfoCar[];
+};
+
+export default function Home({ carsAvailable }: HondaProps) {
   return (
-    <div>
+    <div className={styles.homePage}>
       <Head>
         <title>Home | Honda</title>
       </Head>
+
+      <section className={styles.listCarsContainer}>
+        <h2>Carros Disponíveis</h2>
+        <div className={styles.listCars}>
+          {
+            carsAvailable.map(car => {
+              return (
+                <CardCarAvailable 
+                  key={car.model}
+                  name={car.marketing_name}
+                  logo={car.logo_dark}
+                  thumb={car.thumb}
+                />
+              )
+            })
+          }
+        </div>
+      </section>
     </div>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await fetch(
+    "https://www.honda.com.br/automoveis/sites/hab/files/pps_estados.json"
+  );
+  const data = await res.json();
+
+  const detailsCars = Object.entries(data).map((car) => car[1]);
+
+  const listCars = detailsCars.map((info: InfoCar) => {
+    return {
+      model: info.model,
+      nameMarketing: info.marketing_name,
+      logo_dark: info.logo_dark,
+      thumb: info.thumb,
+    };
+  });
+  
+  const carsAvailable = listCars.filter(car => car.model !== 'Civic Si' )
+
+  return {
+    props: {
+      carsAvailable,
+    },
+  };
+};
